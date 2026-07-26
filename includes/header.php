d8c1f4246170f324fa0b8fa46bedcb2f9bfcec42
iqzahtml <iqzahfarhah@gmail.com>
2026-07-26 22:53:12 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,31 +1,30 @@
 <?php
 
-require_once DIR . "/../database/db.php";
-require_once DIR . "/session.php";
-require_once DIR . "/functions.php";
+require_once DIR . "/../config.php";
 
 ?>
 
 <!DOCTYPE html>
+
 <html lang="en">
 
 <head>
 
     <meta charset="UTF-8">
 
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
 
-    <title>VendorHub</title>
+    <title>
+        VendorHub
+    </title>
 
-    <link rel="stylesheet"
-          href="css/style.css">
-
-    <link rel="stylesheet"
-          href="css/login.css">
-
-    <link rel="stylesheet"
-          href="css/modal.css">
+    <link
+        rel="stylesheet"
+        href="css/style.css"
+    >
 
 </head>
 
