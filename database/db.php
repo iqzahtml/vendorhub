c32c20f5a7d2ec9b6a31259aa6f9a5855b8bdf8c
iqzahtml <iqzahfarhah@gmail.com>
2026-07-26 17:41:12 +0800

    Update db.php

--- database/db.php
+++ database/db.php
@@ -1,12 +1,16 @@
 <?php
+
 $host = "localhost";
-$user = "root";
+$username = "root";
 $password = "";
 $database = "vendorhub";
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($host, $username, $password, $database);
 
-if(!$conn){
+if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
+
+mysqli_set_charset($conn, "utf8mb4");
+
 ?>
