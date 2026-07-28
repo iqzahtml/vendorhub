297fee47b7c3be90b5853d55799a2079266a38e1
iqzahtml <iqzahfarhah@gmail.com>
2026-07-28 23:21:11 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,32 +1,41 @@
 <?php
-
-require_once dirname(__DIR__) . '/config.php';
-
+require_once DIR . '/../config.php';
 ?>
 
 <!DOCTYPE html>
-
 <html lang="en">
 
 <head>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/login.css">
-    <meta charset="UTF-8">
 
-    <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0"
-    >
+<meta charset="UTF-8">
 
-    <title>
-        VendorHub
-    </title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link
-        rel="stylesheet"
-        href="css/style.css"
-    >
+<title><?php echo SITE_NAME; ?></title>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/style.css">
+
+<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/modal.css">
+
+<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/login.css">
+
+<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/product.css">
+
+<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/dashboard.css">
+
+<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/responsive.css">
+
+<link rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 
 </head>
 
 <body>
+
+<div class="wrapper">
