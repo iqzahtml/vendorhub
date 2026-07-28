24c904f54004fae431099539150111e1a8daec1d
iqzahtml <iqzahfarhah@gmail.com>
2026-07-29 00:41:36 +0800

    Update customer_sidebar.php

---
+++ includes/customer_sidebar.php
@@ -0,0 +1,180 @@
+<?php
+/* =====================================================
+   HOCHIPOHUB
+   Customer Sidebar
+===================================================== */
+
+
+?>
+
+
+<aside class="sidebar customer-sidebar">
+
+
+
+<div class="sidebar-header">
+
+
+<i class="fa-solid fa-user"></i>
+
+
+<h3>
+
+Customer Panel
+
+</h3>
+
+
+</div>
+
+
+
+
+
+<ul class="sidebar-menu">
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>dashboard.php">
+
+
+<i class="fa-solid fa-chart-line"></i>
+
+
+Dashboard
+
+
+</a>
+
+</li>
+
+
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>profile.php">
+
+
+<i class="fa-solid fa-user"></i>
+
+
+My Profile
+
+
+</a>
+
+</li>
+
+
+
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>cart.php">
+
+
+<i class="fa-solid fa-cart-shopping"></i>
+
+
+My Cart
+
+
+</a>
+
+</li>
+
+
+
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>order.php">
+
+
+<i class="fa-solid fa-box"></i>
+
+
+My Orders
+
+
+</a>
+
+</li>
+
+
+
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>review.php">
+
+
+<i class="fa-solid fa-star"></i>
+
+
+My Reviews
+
+
+</a>
+
+</li>
+
+
+
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>payment.php">
+
+
+<i class="fa-solid fa-credit-card"></i>
+
+
+Payment History
+
+
+</a>
+
+</li>
+
+
+
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>auth/logout.php">
+
+
+<i class="fa-solid fa-right-from-bracket"></i>
+
+
+Logout
+
+
+</a>
+
+</li>
+
+
+
+
+
+</ul>
+
+
+
+</aside>
