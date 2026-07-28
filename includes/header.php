bf93191248ecc4efa101efd1f6ec265535597584
iqzahtml <iqzahfarhah@gmail.com>
2026-07-29 00:40:39 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,17 +1,74 @@
 <?php
-require_once DIR . '/../config.php';
+/* =====================================================
+   HOCHIPOHUB
+   includes/header.php
+
+   Global Header
+===================================================== */
+
+
+// Load Config
+
+require_once DIR . "/../config.php";
+
+
+// Load Database
+
+require_once DIR . "/../database/db.php";
+
+
+// Load Functions
+
+require_once DIR . "/functions.php";
+
+
+// Load Session
+
+require_once DIR . "/session.php";
+
+
 ?>
 
+
 <!DOCTYPE html>
+
 <html lang="en">
 
 <head>
 
+
 <meta charset="UTF-8">
+
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title><?php echo SITE_NAME; ?></title>
+
+<title>
+
+<?php
+
+echo isset($pageTitle)
+
+?
+$pageTitle." | ".SITE_NAME
+
+:
+
+SITE_NAME;
+
+?>
+
+</title>
+
+
+
+<!-- Logo Icon -->
+
+<link rel="icon" href="<?= BASE_URL ?>image/logo.jpg">
+
+
+
+<!-- Google Font -->
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 
@@ -19,23 +76,47 @@ require_once DIR . '/../config.php';
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
-<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/style.css">
 
-<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/modal.css">
 
-<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/login.css">
+<!-- Font Awesome -->
 
-<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/product.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
-<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/dashboard.css">
 
-<link rel="stylesheet" href="<?php echo BASE_URL; ?>css/responsive.css">
 
-<link rel="stylesheet"
-href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+<!-- Main CSS -->
+
+<link rel="stylesheet" href="<?= BASE_URL ?>css/style.css">
+
+
+<link rel="stylesheet" href="<?= BASE_URL ?>css/modal.css">
+
+
+<link rel="stylesheet" href="<?= BASE_URL ?>css/login.css">
+
+
+<link rel="stylesheet" href="<?= BASE_URL ?>css/responsive.css">
+
+
+<?php if(isset($extraCSS)): ?>
+
+<link rel="stylesheet" href="<?= BASE_URL ?>css/<?= $extraCSS ?>">
+
+<?php endif; ?>
+
 
 </head>
 
+
 <body>
 
-<div class="wrapper">
+
+<?php
+
+
+// Navbar
+
+include DIR . "/navbar.php";
+
+
+?>
