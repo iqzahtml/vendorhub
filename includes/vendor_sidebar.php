f77c53a29bfcce9c7ee12dc6faf0363e45b0f2c0
iqzahtml <iqzahfarhah@gmail.com>
2026-07-29 00:42:06 +0800

    Update vendor_sidebar.php

---
+++ includes/vendor_sidebar.php
@@ -0,0 +1,186 @@
+<?php
+/* =====================================================
+   HOCHIPOHUB
+   Vendor Sidebar
+===================================================== */
+
+
+?>
+
+
+<aside class="sidebar vendor-sidebar">
+
+
+
+<div class="sidebar-header">
+
+
+<i class="fa-solid fa-store"></i>
+
+
+<h3>
+
+Vendor Panel
+
+</h3>
+
+
+</div>
+
+
+
+
+
+
+<ul class="sidebar-menu">
+
+
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>dashboard.php">
+
+
+<i class="fa-solid fa-chart-pie"></i>
+
+
+Dashboard
+
+
+</a>
+
+</li>
+
+
+
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>product.php">
+
+
+<i class="fa-solid fa-box-open"></i>
+
+
+Manage Products
+
+
+</a>
+
+</li>
+
+
+
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>inventory.php">
+
+
+<i class="fa-solid fa-warehouse"></i>
+
+
+Inventory
+
+
+</a>
+
+</li>
+
+
+
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>order.php">
+
+
+<i class="fa-solid fa-receipt"></i>
+
+
+Orders
+
+
+</a>
+
+</li>
+
+
+
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>commission.php">
+
+
+<i class="fa-solid fa-money-bill-transfer"></i>
+
+
+Commission
+
+
+</a>
+
+</li>
+
+
+
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>profile.php">
+
+
+<i class="fa-solid fa-shop"></i>
+
+
+Business Profile
+
+
+</a>
+
+</li>
+
+
+
+
+
+
+<li>
+
+<a href="<?= BASE_URL ?>auth/logout.php">
+
+
+<i class="fa-solid fa-right-from-bracket"></i>
+
+
+Logout
+
+
+</a>
+
+</li>
+
+
+
+
+
+
+</ul>
+
+
+
+
+</aside>
