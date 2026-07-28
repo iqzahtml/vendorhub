6125a3fdfe02881d3b567cf3b2689228b2f8a01a
iqzahtml <iqzahfarhah@gmail.com>
2026-07-29 00:40:36 +0800

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,36 +1,299 @@
 <?php
+/* =====================================================
+   HOCHIPOHUB
+   functions.php
 
-function clean($data)
-{
-    return htmlspecialchars(trim($data));
-}
+   Global Helper Functions
+===================================================== */
 
-function randomCode($length = 6)
-{
-    return substr(str_shuffle("0123456789"), 0, $length);
-}
 
-function uploadImage($file, $folder)
-{
-    if ($file['error'] != 0) {
-        return "";
-    }
 
-    $extension = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+/*
+|--------------------------------------------------------------------------
+| Sanitize Input
+|--------------------------------------------------------------------------
+*/
 
-    $filename = uniqid() . "." . $extension;
+function clean($data){
 
-    move_uploaded_file(
-        $file['tmp_name'],
-        "uploads/$folder/" . $filename
+
+    return htmlspecialchars(
+
+        trim($data),
+
+        ENT_QUOTES,
+
+        'UTF-8'
+
     );
 
-    return $filename;
+
 }
 
-function formatPrice($price)
-{
-    return "RM " . number_format($price, 2);
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Redirect Page
+|--------------------------------------------------------------------------
+*/
+
+function redirect($page){
+
+
+    header(
+
+        "Location: ".$page
+
+    );
+
+
+    exit();
+
+
 }
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Display Alert
+|--------------------------------------------------------------------------
+*/
+
+function alert(
+    $message,
+    $type="info"
+){
+
+
+echo '
+
+<div class="auth-alert '.$type.'">
+
+<i class="fa-solid fa-circle-info"></i>
+
+<span>
+'.$message.'
+</span>
+
+</div>
+
+';
+
+
+}
+
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Check Active Page
+|--------------------------------------------------------------------------
+*/
+
+function activePage($page){
+
+
+$current =
+basename($_SERVER['PHP_SELF']);
+
+
+
+if($current == $page){
+
+    return "active";
+
+}
+
+
+return "";
+
+}
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Format Price
+|--------------------------------------------------------------------------
+*/
+
+function formatPrice($price){
+
+
+return "RM "
+.number_format(
+    $price,
+    2
+);
+
+
+}
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Generate Random OTP
+|--------------------------------------------------------------------------
+*/
+
+function generateOTP(){
+
+
+return rand(
+    100000,
+    999999
+);
+
+
+}
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Password Hash
+|--------------------------------------------------------------------------
+*/
+
+function hashPassword($password){
+
+
+return password_hash(
+
+    $password,
+
+    PASSWORD_DEFAULT
+
+);
+
+
+}
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Verify Password
+|--------------------------------------------------------------------------
+*/
+
+function verifyPassword(
+    $password,
+    $hash
+){
+
+
+return password_verify(
+
+    $password,
+
+    $hash
+
+);
+
+
+}
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Upload Image
+|--------------------------------------------------------------------------
+*/
+
+function uploadImage(
+    $file,
+    $folder
+){
+
+
+
+$allowed = [
+
+    "jpg",
+    "jpeg",
+    "png",
+    "webp"
+
+];
+
+
+
+$filename =
+$file['name'];
+
+
+
+$extension =
+strtolower(
+    pathinfo(
+        $filename,
+        PATHINFO_EXTENSION
+    )
+);
+
+
+
+if(
+    !in_array(
+        $extension,
+        $allowed
+    )
+){
+
+    return false;
+
+}
+
+
+
+
+$newName =
+uniqid()
+."."
+.$extension;
+
+
+
+$destination =
+$folder
+.$newName;
+
+
+
+if(
+    move_uploaded_file(
+        $file['tmp_name'],
+        $destination
+    )
+){
+
+    return $newName;
+
+}
+
+
+
+return false;
+
+
+
+}
+
+
 
 ?>
