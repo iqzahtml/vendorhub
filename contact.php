c0910161539de15a36eef9b11ef96f491d07be5c
iqzahtml <iqzahfarhah@gmail.com>
2026-07-28 23:27:04 +0800

    Update contact.php

---
+++ contact.php
@@ -0,0 +1,70 @@
+<?php
+
+include 'includes/header.php';
+include 'includes/navbar.php';
+
+?>
+
+<section class="contact-page">
+
+<div class="container">
+
+<h1>Contact Us</h1>
+
+<p>
+
+Need help? Contact the HochipoHub team.
+
+</p>
+
+<form>
+
+<div class="input-group">
+
+<label>Full Name</label>
+
+<input type="text">
+
+</div>
+
+<div class="input-group">
+
+<label>Email</label>
+
+<input type="email">
+
+</div>
+
+<div class="input-group">
+
+<label>Subject</label>
+
+<input type="text">
+
+</div>
+
+<div class="input-group">
+
+<label>Message</label>
+
+<textarea rows="6"></textarea>
+
+</div>
+
+<button class="btn-login">
+
+Send Message
+
+</button>
+
+</form>
+
+</div>
+
+</section>
+
+<?php
+
+include 'includes/footer.php';
+
+?>
