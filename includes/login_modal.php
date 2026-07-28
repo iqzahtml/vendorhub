e9dad3997adc3466e7e4a1ff29dc9c9b8bf6abf6
qilardzuan <qilardzuan16@gmail.com>
2026-07-28 14:29:40 +0800

    Update login_modal.php

--- includes/login_modal.php
+++ includes/login_modal.php
@@ -1,47 +1,67 @@
 <div id="loginModal" class="modal">
 
-    <div class="login-box">
+    <div class="modal-content">
 
-        <span class="close" onclick="closeLoginModal()">&times;</span>
+        <button class="close" onclick="closeLoginModal()">
+            &times;
+        </button>
 
-        <h2 class="logo">VendorHub</h2>
+        <div class="auth-logo">
+            Vendor<span>Hub</span>
+        </div>
 
-        <p class="subtitle">Welcome Back</p>
+        <h2 class="modal-title">
+            Welcome Back
+        </h2>
+
+        <p class="modal-subtitle">
+            Login to continue shopping
+        </p>
 
         <form action="login.php" method="POST">
 
             <div class="form-group">
+
                 <label>Email</label>
+
                 <input
                     type="email"
                     name="email"
+                    class="form-control"
                     placeholder="Enter your email"
                     required>
+
             </div>
 
             <div class="form-group">
+
                 <label>Password</label>
+
                 <input
                     type="password"
                     name="password"
+                    class="form-control"
                     placeholder="Enter your password"
                     required>
+
             </div>
 
-            <button type="submit" class="login-btn">
-                LOGIN
+            <button
+                type="submit"
+                class="login-button">
+
+                Login
+
             </button>
 
         </form>
 
-        <div class="bottom-text">
+        <div class="modal-register">
 
             Don't have an account?
 
             <a href="register.php">
-
-                Register
-
+                Register Here
             </a>
 
         </div>
