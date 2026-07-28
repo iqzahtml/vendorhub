dac8610c68f8a6d0ab33b361fb91759b68721d63
iqzahtml <iqzahfarhah@gmail.com>
2026-07-28 23:16:57 +0800

    Update db.php

--- database/db.php
+++ database/db.php
@@ -1,16 +1,15 @@
 <?php
 
 $host = "localhost";
-$username = "root";
+$user = "root";
 $password = "";
-$database = "vendorhub";
+$database = "hochipohub";
 
-$conn = mysqli_connect($host, $username, $password, $database);
+$conn = mysqli_connect($host, $user, $password, $database);
 
 if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
+    die("Database Connection Failed : " . mysqli_connect_error());
 }
 
-mysqli_set_charset($conn, "utf8mb4");
-
+mysqli_set_charset($conn, "utf8");
 ?>
